9fc474af9ca079aed205ad36570ad29a79b8bd99
Yusniel Brocard <brocard@gmail.com>
2017-11-15 11:59:52 -0600

    initial files like Facade and ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Driizl\Elastic;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
